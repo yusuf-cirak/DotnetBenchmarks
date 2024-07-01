afa1d4669f5081abecdc60c2dadadc654ba8b6cd
Yusuf Çırak <yusuf10901@hotmail.com>
2024-07-01 13:22:42 +0300

    initial commit

---
+++ Exceptions/ExceptionVsReturn.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Exceptions;
+
+[MemoryDiagnoser]
+public class ExceptionVsReturn
+{
+    const string Message = "Message";
+    
+    public Func<string> GetFunc()
+    {
+        return delegate
+        {
+            var exception = new Exception(Message);
+            return exception.Message;
+        };
+    }
+
+    [Benchmark]
+    public string ReturnString_WithoutThrowingException()
+    {
+        var func = GetFunc();
+        return func();
+    }
+
+    [Benchmark]
+    public string ReturnString_WithThrowingException()
+    {
+        try
+        {
+            throw new Exception(Message);
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+
+
+}
